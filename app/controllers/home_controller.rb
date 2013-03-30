7b22a4a398281b5ff7a53f9d2332ca0ec9bf9b31
John Kealy <jdkealy@gmail.com>
2013-03-30 19:31:30 -0400

    disable emails temporarily

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def contact
   end
   def submit_inquiry
-    AppMailer.contact(params).deliver
+    #AppMailer.contact(params).deliver
     flash[:notice] = "Thanks for your inquiry! I'll get back to you as soon as I can!"
     redirect_to :back
   end
